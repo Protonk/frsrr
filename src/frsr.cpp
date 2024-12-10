d103f4f22a175ad747581b9d3c8a4ab380adf610
hyland-uw <protonk@gmail.com>
2024-12-10 11:08:03 -0800

    working parallel functions

--- src/frsr.cpp
+++ src/frsr.cpp
@@ -1,170 +1,137 @@
 #include <Rcpp.h>
 #include <cmath>
 #include <cstdint>
+#include <RcppParallel.h>
 using namespace Rcpp;
-//' Fast Reciprocal Square Root (FRSR)
-//'
-//' A parameterized Fast Reciprocal Square Root algorithm written in C++.
-//'
-//' @param x A numeric vector of input values.
-//' @param magic An unsigned 32-bit integer restoring constant. Default is 0x5f3759df.
-//' @param NR An integer specifying the maximum number of Newton-Raphson iterations. Default is 1.
-//' @param A Parameter for the Newton-Raphson iteration. Default is \code{1.5}.
-//' @param B Parameter for the Newton-Raphson iteration. Default is \code{0.5}.
-//' @param tol The absolute relative error at which to stop early. Default is 0 (no early stopping).
-//'
-//' @return 
-//' \code{frsr} returns a numeric vector of the same length as \code{x},
-//' containing the \code{final} approximations of 1/sqrt(x).
-//'
-//' \code{frsr.detail} returns a data frame with columns:
-//'     \item{input}{The input values}
-//'     \item{magic}{The integer restoring constant chosen}
-//'     \item{initial}{Initial approximation from integer operations}
-//'     \item{after_one}{Result after one iteration of Newton-Raphson}
-//'     \item{final}{Result from final iteration}
-//'     \item{error}{Absolute relative error of final versus standard library}
-//'     \item{diff}{Difference between final and penultimate approximations}
-//'     \item{iters}{Number of iterations performed}
-//'
-//' @details
-//' The function supplies a Fast Reciprocal Square Root algorithm, which provides
-//' an approximation of 1/sqrt(x). The user can specify their own parameters. The
-//' default values are set to those used by the famous "fast inverse square
-//' root" in Quake III Arena.
-//'
-//' The algorithm exploits the fact that the integer representation of a
-//' floating-point number offers a piecewise linear approximation to the
-//' logarithm function. Right-shifting the integer bits of a float is
-//' equivalent to dividing the logarithm of the number by two.
-//' By subtracting this from a carefully chosen constant, an approximation
-//' of \eqn{-1/2 * log2(x)} can be obtained. Treating that result as a float
-//' again by using integer bits in memory gives a good guess of
-//' \eqn{exp(-1/2 * log2(x))}, which is \eqn{1/sqrt(x)}.
-//'
-//' The "magic" constant principally serves to restore the exponent bits lost
-//' when the input float is right shifted. A restoring constant which does only
-//' that is `0x5F400000`, given by Blinn 1997. Values of magic from roughly
-//' `0x5f2ffb20` to `0x5f404ed0` will give acceptable levels of error.
-//'
-//' The Newton-Raphson step \eqn{y_{n+1} = y_n * (1.5 - 0.5 * x * y_n^2)}
-//' is performed repeatedly until the specified maximum NR is reached. The
-//' default is one. Grossly different values of magic from the default may
-//' require many iterations to approach the correct output.
-//'
-//' Parameters in the Newton-Raphson step, \eqn{(A - B * x * y_n^2)} need
-//' not be fixed at 1.5 and 0.5 and can be set by the user. Note that
-//' if B =/= A - 1, the approximation may fail to converge.
-//'
-//' @references
-//' J. F. Blinn, (July-Aug. 1997) "Floating-point tricks," in IEEE Computer Graphics and Applications, vol. 17, no. 4, pp. 80-84 \doi{10.1109/38.595279}
-//'
-//' J. T. Coonen, (1984) §2.3 "A Poor Man's Logarithm" in Contributions to a Proposed Standard for Binary Floating-Point Arithmetic. PhD Thesis, University of California Berkeley
-//'
-//' S. Summit, (2023). Answer to "Why does the integer representation of a floating point number offer a piecewise linear approximation to the logarithm?" Stack Overflow. \url{https://stackoverflow.com/a/75772363/1188479}
-//'
-//' @examples
-//' \dontrun{
-//' # Custom parameters
-//' result <- frsr(c(1, 4, 9, 16), magic = 0x5f375a86, NR = 2, A = 1.6, B = 0.6)
-//' ## result is a vector of length 4
-//' print(result)
-//' # [1] 0.9990148 0.4995074 0.3337626 0.2497537
-//'
-//' # Optional detail
-//' result.df <- frsr.detail(c(pi, 2^-31, 0.4, 6.02e23))
-//' ## result.df is a dataframe with 3 rows and 8 columns
-//' print(result.df)
-//' #          input      magic      initial    after_one        final        error          diff iters
-//' # 1 3.141593e+00 1597463007 5.735160e-01 5.639570e-01 5.639570e-01 0.0004121269 -9.558976e-03     1
-//' # 2 4.656613e-10 1597463007 4.693787e+04 4.632937e+04 4.632937e+04 0.0002499308 -6.085039e+02     1
-//' # 3 4.000000e-01 1597463007 1.632430e+00 1.578616e+00 1.578616e+00 0.0015955754 -5.381417e-02     1
-//' # 4 6.020000e+23 1597463007 1.306493e-12 1.288484e-12 1.288484e-12 0.0002824810 -1.800936e-14     1
-//' }
-//'
+using namespace RcppParallel;
+
+struct FRSRWorker : public Worker {
+  const RVector<double> x;
+  const uint32_t magic;
+  const int NR;
+  const float A, B, tol;
+  RVector<double> result;
+
+  FRSRWorker(const NumericVector x, uint32_t magic, int NR, float A, float B, float tol, NumericVector result)
+    : x(x), magic(magic), NR(NR), A(A), B(B), tol(tol), result(result) {}
+
+  void operator()(std::size_t begin, std::size_t end) {
+    for (std::size_t j = begin; j < end; ++j) {
+      float x_val = x[j];
+      float reference = 1 / std::sqrt(x_val);
+      float rel_error = std::numeric_limits<float>::max();
+
+      // A union here allows us to represent the same memory bits
+      // as both a float and an unsigned integer, giving us access
+      // to the "evil floating point bit level hacking" of 
+      // the https://en.wikipedia.org/wiki/Fast_inverse_square_root
+      // without invoking undefined behavior.
+      // "[T]]he bit pattern of a floating point number, 
+      //  interpreted as an integer, gives a piecewise linear 
+      //  approximation to the logarithm function"
+      //  From Jim Blinn's "Floating-point tricks" (1997)
+      //  The first step to get 1/sqrt(x) is to get -1/2 * log2(x),
+      //  this is our "poor man's" logarithm.
+      union {
+        float f;
+        uint32_t u;
+      } y = {x_val};
+
+      // The magic constant is largely a restoring constant,
+      // restoring the exponent bits lost when the float is right shifted.
+      // But a specially chosen constant can give a better first guess.
+      // This divides by -2, so now we have a guess at -1/2 * log2(x).
+      y.u = static_cast<uint32_t>(magic) - (y.u >> 1);
+
+      // set NR to 0 to skip iteration entirely
+      for (int i = 1; i <= NR; i++) {
+        // Blink and you'll miss it. Moving from y.u to y.f takes us
+        // out of the logarithmic domain and approximates an exponential.
+        // exp(-1/2 * log2(x)) = 1/sqrt(x), so that's our approximation
+        // to feed into Newton's method.
+        y.f = y.f * (A - B * x_val * y.f * y.f);
+        
+        // Tolerance of 0 and below are ignored
+        if (tol > 0) {
+          rel_error = std::abs(y.f - reference) / reference;
+          if (rel_error <= tol) {
+            break;
+          }
+        }
+      }
+
+      result[j] = y.f;
+    }
+  }
+};
+
+struct FRSRDetailWorker : public Worker {
+  const RVector<double> x;
+  const uint32_t magic;
+  const int NR;
+  const float A, B, tol;
+  RVector<double> initial, after_one, final, error, diff;
+  RVector<int> iters_vec;
+
+  FRSRDetailWorker(const NumericVector x, uint32_t magic, int NR, float A, float B, float tol,
+                   NumericVector initial, NumericVector after_one, NumericVector final,
+                   NumericVector error, NumericVector diff, IntegerVector iters_vec)
+    : x(x), magic(magic), NR(NR), A(A), B(B), tol(tol),
+      initial(initial), after_one(after_one), final(final),
+      error(error), diff(diff), iters_vec(iters_vec) {}
+
+  void operator()(std::size_t begin, std::size_t end) {
+    for (std::size_t j = begin; j < end; ++j) {
+      float x_val = x[j];
+      int actual_iters = 0;
+      float reference = 1 / std::sqrt(x_val);
+      float rel_error = std::numeric_limits<float>::max();
+
+      union {
+        float f;
+        uint32_t u;
+      } y = {x_val};
+
+      y.u = static_cast<uint32_t>(magic) - (y.u >> 1);
+      initial[j] = y.f;
+
+      float prev_y = y.f;
+
+      for (int i = 1; i <= NR; i++) {
+        actual_iters++;
+        y.f = y.f * (A - B * x_val * y.f * y.f);
+        rel_error = std::abs(y.f - reference) / reference;
+
+        if (i == 1) {
+          after_one[j] = y.f;
+        }
+
+        if (tol > 0 && rel_error <= tol) {
+          break;
+        }
+      }
+      diff[j] = y.f - prev_y;
+      final[j] = y.f;
+      error[j] = rel_error;
+      iters_vec[j] = actual_iters;
+    }
+  }
+};
 // [[Rcpp::export]]
 NumericVector frsr(NumericVector x, uint32_t magic = 0x5f3759df, int NR = 1, float A = 1.5, float B = 0.5, float tol = 0) {
-    int n = x.size();
-    NumericVector result(n);
-
-    for (int j = 0; j < n; ++j) {
-        float x_val = x[j];
-        float reference = 1 / std::sqrt(x_val);
-        float rel_error = std::numeric_limits<float>::max();
-
-        union {
-            float f;
-            uint32_t u;
-        } y = {x_val};
-
-        y.u = static_cast<uint32_t>(magic) - (y.u >> 1);
-
-        for (int i = 1; i <= NR; i++) {
-            y.f = y.f * (A - B * x_val * y.f * y.f);
-            rel_error = std::abs(y.f - reference) / reference;
-
-            if (tol > 0 && rel_error <= tol && i >= 1) {
-                break;
-            }
-        }
-        
-        result[j] = y.f;
-    }
-
-    return result;
+  NumericVector result(x.size());
+  FRSRWorker frsrWorker(x, magic, NR, A, B, tol, result);
+  parallelFor(0, x.size(), frsrWorker);
+  return result;
 }
 // [[Rcpp::export]]
 DataFrame frsr_detail(NumericVector x, uint32_t magic = 0x5f3759df, int NR = 1, float A = 1.5, float B = 0.5, float tol = 0) {
   int n = x.size();
-  NumericVector input(n), initial(n), after_one(n), final(n), error(n), diff(n);
+  NumericVector initial(n), after_one(n), final(n), error(n), diff(n);
   IntegerVector magic_vec(n, magic), iters_vec(n);
 
-  for (int j = 0; j < n; ++j) {
-    float x_val = x[j];
-    int actual_iters = 0;
-    float reference = 1 / std::sqrt(x_val);
-    float rel_error = std::numeric_limits<float>::max();
-    // Access bits of the float and treat them as bits in memory of an unsigned int
-    // A union is used to reinterpret the float bits as an integer,
-    // allowing for bit manipulation without violating strict aliasing rules.
-    union {
-      float f;
-      uint32_t u;
-    } y = {x_val};
-    // Right shift the integer, which is the equivalent of log2(x)/2
-    // See https://stackoverflow.com/q/75772363/1188479
-    // subtract from a restoring constant to return exponent
-    // bits lost in the shift & make that -log2(x)/2
-    y.u = static_cast<uint32_t>(magic) - (y.u >> 1);
-    // back as a float, this is exp(-log2(x)/2) ~ 1/sqrt(x)
-    initial[j] = y.f;
-
-    // this is to track lagged differences
-    float prev_y = y.f;
-
-    for (int i = 1; i <= NR; i++) {
-      // tracking this rather than i lets us not worry about
-      // where our break statements are
-      actual_iters++;
-      y.f = y.f * (A - B * x_val * y.f * y.f);
-      rel_error = std::abs(y.f - reference) / reference;
-      // capturing the first and last iteration can be handy
-      // without needing to store all of them
-      if (i == 1) {
-        after_one[j] = y.f;
-      }
-
-      // A user may want to iterate to a tolerance
-      // or a fixed number of iterations
-      if (tol > 0 && rel_error <= tol && i >= 1) {
-        break;
-      }
-    }
-    diff[j] = y.f - prev_y;
-    final[j] = y.f;
-    error[j] = rel_error;
-    iters_vec[j] = actual_iters;
-  }
+  FRSRDetailWorker frsrDetailWorker(x, magic, NR, A, B, tol, initial, after_one, final, error, diff, iters_vec);
+  parallelFor(0, n, frsrDetailWorker);
 
   return DataFrame::create(
     _["input"] = x,
