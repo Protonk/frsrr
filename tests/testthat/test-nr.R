3d8b2cfd799cf17794ee71991ed28f872494448c
hyland-uw <protonk@gmail.com>
2024-12-19 10:53:46 -0800

    tests working with frsr_NR

--- tests/testthat/test-nr.R
+++ tests/testthat/test-nr.R
@@ -1,20 +1,21 @@
-test_that("ynplusone works with standard formula", {
+# FILE: tests/testthat/test-nr.R
+
+test_that("frsr_NR works with standard formula", {
     x <- c(1, 4, 9, 16)
     guess <- c(1, 0.5, 0.333, 0.25)
     formula <- quote(y * (1.5 - 0.5 * x * y^2))
-    result <- ynplusone(x, guess, formula)
-    expect_equal(result, frsr(x), tolerance = 1e-2)
+    result <- frsr_NR(x, formula = formula, NRmax = 1)
+    expect_equal(result$final, frsr(x), tolerance = 1e-2)
 })
 
-test_that("ynplusone works with different formulae", {
+test_that("frsr_NR works with different formulae", {
     x <- c(1, 4, 9, 16)
-    guess <- c(1, 0.5, 0.333, 0.25)
     ## not likely to be accurate, but should return a result
-    wrong <- ynplusone(x, guess, quote(y * (1.4 - 0.4 * x - y^2)))
+    wrong <- frsr_NR(x, formula = quote(y * (1.4 - 0.4 * x - y^2)), NRmax = 1)
     ## just to make sure we're not bamboozling ourselves
     std_formula <- quote(y * (1.5 - 0.5 * x * y^2))
-    std_result <- ynplusone(x, guess, std_formula)
-    expect_false(isTRUE(abs(wrong - std_result) < 1e-3))
+    std_result <- frsr_NR(x, formula = std_formula, NRmax = 1)
+    expect_false(isTRUE(abs(wrong$final - std_result$final) < 1e-3))
 })
 
 test_that("frsr_NR works with default parameters", {
@@ -38,4 +39,3 @@ test_that("frsr_NR produces expected output structure", {
     expect_s3_class(result, "data.frame")
     expect_equal(nrow(result), length(x))
 })
-
